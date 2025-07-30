8187d95341d3f375c25967be16ea28762491efe0
Morcado <gposcarr18@gmail.com>
2025-07-30 13:20:08 -0600

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,15 @@
+import { NuxtPage } from '../.nuxt/components';
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+
+body {
+  background-image: url('../app/assets/background/full-background.png') ;
+  background-size:cover;
+}
+
+</style>
