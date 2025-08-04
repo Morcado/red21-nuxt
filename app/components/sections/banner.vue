cf4077ad3820077e0eded22951c79ff1e0550765
Oscar Gonzalez <sefin.oscar.gonzalez@slp.gob.mx>
2025-08-04 09:44:43 -0600

    add principal banner

---
+++ app/components/sections/banner.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="grid grid-cols-3 grid-rows-3 justify-between">
+    <div class="row-span-3 flex justify-center ">
+      <img class="animation-logo" src="../../assets/images/CIRCULO.png" alt="logo">
+    </div>
+    <div class="col-span-2">
+      <div class="">
+        <svg id="Layer_1" data-name="Layer 1" viewBox="0 0 864.51 190.45">
+          <path class="cls-1"
+            d="M0,138.58V48.24S.51,38,8.49,38H81.33s19.94,1.8,19.94,20.59S102.82,100.87,67,106l38.09,32.56H77.34L39.63,105.76H25.87V87.62H67.56S81.71,87,83.13,74.49V59.18s-.26-3.86-3.35-3.86H20.46a2.69,2.69,0,0,0-2.44,3v80.3Z" />
+          <path class="cls-1"
+            d="M210.4,55.58H137.3v58.68a7.78,7.78,0,0,0,8.24,6.82H210.4v17.5H143.48s-23.93-1.42-23.93-23.68V45.67s1-7.72,8-7.72H210.4Z" />
+          <rect class="cls-1" x="145.54" y="79.25" width="57.26" height="18.02" />
+          <path class="cls-1"
+            d="M293.14,38.59H238.19s-6.69-.77-9,6.95v84.54s0,8.5,9,8.5h52.89s37.83-.91,37.83-32.82V73.08S325.57,43,293.14,38.59Zm18.14,65c-3.86,17.88-23.93,17.5-23.93,17.5H246.94V55.45h44.14c12.74,0,18.79,12.87,20.2,17.63S312.31,84.92,311.28,103.58Z" />
+          <path class="cls-1"
+            d="M464.29,138.58H392.45V102.51s1.64-23,22.49-23h44s7.81.2,7.91-7.91,0-9.84,0-9.84-1.25-6.76-7-6.57-59,0-59,0V37.85h62.34S483,40.17,484.91,60s-3.19,37-26.73,37.45-39.29,0-39.29,0-8.78-1.42-8.58,8.36.19,15.48.19,15.48h74.31v17.34Z" />
+          <polygon class="cls-1"
+            points="533.13 138.58 533.13 37.85 499.03 37.85 499.03 55.32 516.08 55.32 516.08 138.58 533.13 138.58" />
+          <path class="cls-2"
+            d="M785.82,89.77C782.19,85.4,699.4,3.35,699.4,3.35s-6-7-13.21,0-11.84,11.4-11.84,11.4L599.59,89.52s-3.86,5.79,0,10.29,88.72,88.73,88.72,88.73,5.47,4.31,9.59,0,87.92-87.92,87.92-87.92S789.46,94.15,785.82,89.77Zm-93,62.74L635.49,95.18l56.8-57.47L750,95.37Z" />
+          <path class="cls-2"
+            d="M861.42,88.81S777.51,7.59,772.54,2.62s-11-1.35-11-1.35-6,5.89-9.94,9.94,0,9.17,0,9.17l76.15,75-73.35,70.87C745.5,174.8,753,180.5,753,180.5l7.85,7.84c4.37,4.38,11.45,0,11.45,0s82.19-80.33,89.14-87.08S861.42,88.81,861.42,88.81Z" />
+        </svg>
+      </div>
+      <div class="col-span-2 row-start-2">
+        <h3 class="text-white"><b>Tecnología avanzada</b><br>para tus necesidades</h3>
+        <div class="flex flex-row">
+          <img class="mx-2 small-image" src="../../assets/images/negocio.png" alt="negocio">
+          <img class="mx-2 small-image" src="../../assets/images/hogar.png" alt="hogar">
+          <img class="mx-2 small-image" src="../../assets/images/empresa.png" alt="negocio">
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+  .cls-1 {
+    fill: #fff;
+  }
+
+  .cls-2 {
+    fill: #ed2024;
+  }
+
+
+  /* mobile */
+@media only screen and (max-width: 992px) {
+  .small-image {
+    height: 100px;
+  }
+
+  .animation-logo {
+    z-index: -10;
+    height: 300px;
+    display: block;
+    margin-top: -250px;
+    margin-left: auto;
+    margin-right: auto;
+    overflow: hidden;
+    position: relative;
+
+    -webkit-animation:spin 50s linear infinite;
+    -moz-animation:spin 50s linear infinite;
+    animation:spin 50s linear infinite;
+  }
+
+  
+}
+
+  /* desktop */
+@media only screen and (min-width: 992px) {
+
+  .animation-logo {
+    height: 500px;
+    margin-left: -250px;
+    overflow: hidden;
+    position: relative;
+
+    -webkit-animation:spin 50s linear infinite;
+    -moz-animation:spin 50s linear infinite;
+    animation:spin 50s linear infinite;
+  }
+
+  
+
+}
+
+  @-moz-keyframes spin { 70% { -moz-transform: rotate(360deg); } }
+  @-webkit-keyframes spin { 70% { -webkit-transform: rotate(360deg); } }
+  @keyframes spin { 70% { -webkit-transform: rotate(360deg); transform:rotate(360deg); } }
+
+</style>
