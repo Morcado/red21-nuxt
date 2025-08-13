fc973f13f69be68978ee4efc4f53163a4ed70510
Oscar Gonzalez <sefin.oscar.gonzalez@slp.gob.mx>
2025-08-13 15:03:00 -0600

    reubicacion textos

--- app/components/sections/about.vue
+++ app/components/sections/about.vue
@@ -1,22 +1,42 @@
 <template>
-  <div id="acerca-de" class="grid md:grid-cols-5 gap-4 md:p-4 xs: my-25 md:my-40">
-    <div class="md:col-start-2 col-span-1">
-      <h1 class="md:text-right text-4xl text-white text-center">Nosotros</h1>
+  <div id="acerca-de" class="grid md:grid-cols-4 gap-4 md:p-4 xs: my-25 md:my-40">
+    <div class="md:col-start-1 md:col-span-5 ">
+      <h1 class=" text-4xl text-white text-center">Nosotros</h1>
     </div>
-    <div class="md:col-span-2">
-      <p class="text-white text-left">
-        Somos una empresa dedicada a las
-        telecomunicaciones y servicios de consultaría en TI.
+    <div class="md:col-start-2 md:col-span-2">
+      <h4 class="text-white text-2xl">Misión</h4>
+      <p class="text-white text-left text-md/6">
+        Nuestra misión es proporcionar soluciones de telecomunicaciones innovadoras y confiables que conecten a las
+        personas, empresas y comunidades en el área del altiplano de san Luis potosí y en el estado de zacatecas.
       </p>
-      <p class="text-white text-left">
-        Nuestra misión es ofrecer soluciones de telecomunicaciones de vanguardia que permitan la comunicación efectiva y
-        el acceso a la información en cualquier momento y lugar.
+    </div>
+    <div class="md:col-start-3 md:row-start-3 md:col-span-1 ">
 
+      <br>
+      <h4 class="text-white text-2xl">Visión</h4>
+      <p class="text-white text-left text-md/6">
+        Nuestra visión es construir un San Luis Potosí completamente conectado, donde la comunicación sin fronteras sea
+        una realidad para todos.
+        Ser el líder indiscutible en soluciones de telecomunicaciones, proporcionando servicios innovadores y de alta
+        calidad que mejoren la vida de las personas y las empresas.
+        Superar los límites de la conectividad, llevando la tecnología a nuevas alturas a través de la inversión en
+        infraestructura de red de vanguardia y el desarrollo de soluciones de comunicación avanzadas.
+        Visualizarnos como una empresa impulsada por la excelencia, centrada en el cliente y comprometida con la
+        sostenibilidad y la responsabilidad social.
       </p>
-      <p class="text-white text-left font-gadugi">
-        A través de nuestra infraestructura avanzada y nuestro compromiso con la excelencia, buscamos conectar a las
-        personas, empresas y comunidades, fomentando el progreso, la colaboración y el desarrollo socio económico.
+    </div>
+    <div class="md:col-start-2 md:row-start-3 md:col-span-1">
 
+      <br>
+      <h4 class="text-white text-2xl">Objetivos</h4>
+      <p class="text-white text-left text-md/6">
+        Ofrecer servicios de alta calidad que mejoren la conectividad de nuestros clientes, al tiempo que impulsamos el
+        desarrollo económico y social a través de la conectividad global.
+        Ser líderes en tecnología, brindando infraestructura de red de última generación, servicios de comunicaciones
+        eficientes y soluciones integrales adaptadas a las necesidades cambiantes del mercado.
+        ser el facilitador de la transformación digital, capacitando a las personas para que se conecten, crezcan y
+        prosperen en un mundo cada vez más interconectado.
+        Trabajar incansablemente para superar las expectativas de nuestros clientes y marcar la diferencia.
       </p>
     </div>
   </div>
